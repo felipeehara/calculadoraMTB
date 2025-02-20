fe7bc0d985d4f70762831c01ec89bc2e0aa74e4b
Felipe Ehara <felipeyoug1@gmail.com>
2025-02-20 16:37:10 -0300

    Cor do grafico alterado

--- src/app/components/BarChart.tsx
+++ src/app/components/BarChart.tsx
@@ -27,14 +27,16 @@ const BarChart = ({ proteina, carboidratos, gordura }: BarChartProps) => {
           label: 'Macronutrientes',
           data: [proteina, carboidratos, gordura],
           backgroundColor: [
-            'rgba(255, 99, 132, 0.7)', // Cor para Proteínas (vermelho)
-            'rgba(54, 162, 235, 0.7)', // Cor para Carboidratos (azul)
+            'rgba(54, 162, 235, 0.7)', // Cor para Proteínas (vermelho)
+            'rgba(255, 99, 132, 0.7)', // Cor para Carboidratos (azul)
             'rgba(255, 206, 86, 0.7)', // Cor para Gorduras (amarelo)
+             
           ],
           borderColor: [
-            'rgba(255, 99, 132, 1)',
             'rgba(54, 162, 235, 1)',
+            'rgba(255, 99, 132, 1)',
             'rgba(255, 206, 86, 1)',
+            
           ],
           borderWidth: 2,
         },
